b24b325d3a0be6981350f84331e4b5f177cfa2a0
emanuele <emanuele971@icloud.com>
2024-09-05 19:24:54 +0200

    Rimuovi dist da .gitignore per il deploy su Heroku

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,22 +1,27 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import 'reflect-metadata';
 import { NotebookModule } from './notebook/notebook.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as dotenv from 'dotenv';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(NotebookModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+  dotenv.config();
   const corsOptions: CorsOptions = {
-    origin: ['http://localhost:4200', 'http://localhost:4300'],
+    origin: [
+      'http://localhost:4200', 
+      'http://localhost:4300', 
+      process.env.DOMAIN
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Se necessario per autenticazione con cookie o token
   };
+  
   app.enableCors(corsOptions);
   await app.listen(port);
   Logger.log(
